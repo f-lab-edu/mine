a0c464a835e4e16ea32931908e09cf7ba8430908
leeseowoo <leeseowoo.kr@gmail.com>
2022-09-12 22:32:33 +0900

    #11 feat: 최고 입찰가 갱신 실패 exception 생성

--- src/main/java/com/mine/common/response/CommonControllerAdvice.java
+++ src/main/java/com/mine/common/response/CommonControllerAdvice.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-
 @RestControllerAdvice
 @Slf4j
 public class CommonControllerAdvice {
--- src/main/java/com/mine/common/response/ErrorCode.java
+++ src/main/java/com/mine/common/response/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
     COMMON_INVALID_PARAMETER("요청한 값이 올바르지 않습니다."),
     COMMON_ENTITY_NOT_FOUND("존재하지 않는 엔티티입니다."),
     COMMON_ILLEGAL_STATUS("잘못된 상태 값입니다."),
-    COMMON_ENTITY_EXISTS("이미 존재하는 엔티티입니다.");
+    COMMON_ENTITY_EXISTS("이미 존재하는 엔티티입니다."),
+    COMMON_HIGHEST_BID_NOT_UPDATED("입찰에 실패했습니다. 현재 최고 입찰가보다 높은 입찰가로 다시 시도해주세요.");
 
     private final String errorMessage;
 
